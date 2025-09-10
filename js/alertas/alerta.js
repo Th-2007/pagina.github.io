f7385d6637a48140b1a6d5302a25a71d651433ea
Th-2007 <thiagovab13@gmail.com>
2025-09-10 10:22:05 -0300

    tiempo divvv

--- js/alertas/alerta.js
+++ js/alertas/alerta.js
@@ -1,4 +1,4 @@
 setTimeout(() => {
 const alertDiv = document.getElementById('welcomeAlert');
 if (alertDiv) alertDiv.style.display = 'none';
-}, 2000); // Desaparece después de 4 segundos
+}, 1000); // Desaparece después de 4 segundos
